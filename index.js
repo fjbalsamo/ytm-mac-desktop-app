1618dafa631a810000a4739d853fa34ff886b0bc
Franco Balsamo <fjbalsamo@gmail.com>
2025-03-18 17:09:13 -0300

    1st commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const { app, BrowserWindow } = require('electron');
+
+app.whenReady().then(() => {
+    let win = new BrowserWindow({
+        width: 1200,
+        height: 800,
+        webPreferences: { nodeIntegration: false }
+    });
+
+    win.loadURL('https://music.youtube.com');
+});
